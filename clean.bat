e906484637718d76d3f02f227380fd7767ad7c2e
jbikker <bikker.j@gmail.com>
2022-11-15 18:17:57 +0100

    Prepped for the lecture.

---
+++ clean.bat
@@ -0,0 +1,29 @@
+rd .vs /S /Q
+
+cd "1. Basics"
+rd x64\debug /S /Q
+rd x64\release /S /Q
+rd x64 /S /Q
+del buildlog.txt
+cd ..
+
+cd "2. Whitted"
+rd x64\debug /S /Q
+rd x64\release /S /Q
+rd x64 /S /Q
+del buildlog.txt
+cd ..
+
+cd "3. Cook"
+rd x64\debug /S /Q
+rd x64\release /S /Q
+rd x64 /S /Q
+del buildlog.txt
+cd ..
+
+cd "4. Kajiya"
+rd x64\debug /S /Q
+rd x64\release /S /Q
+rd x64 /S /Q
+del buildlog.txt
+cd ..
